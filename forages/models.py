bdc53dca194c9d583eda8cdbcad65f381eaa61b5
servedredt0mat0 <ryszard.bialach2@gmail.com>
2020-12-07 13:45:35 -0500

    ...

--- forages/models.py
+++ forages/models.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.contrib.gis.db import models
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
