4615a532eb6f3f92442080237810415fe933e945
servedredt0mat0 <ryszard.bialach2@gmail.com>
2020-12-08 00:39:08 -0500

    added loginmixin

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, UpdateView
 from .forms import CustomUserCreationForm, CustomUserChangeForm
@@ -11,11 +12,11 @@ class SignUpView(CreateView):
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
 
-class ProfileView(DetailView):
+class ProfileView(DetailView, LoginRequiredMixin):
     model = CustomUser 
     template_name = 'profile/profile.html'
 
-class ChangeProfileView(UpdateView):
+class ChangeProfileView(UpdateView, LoginRequiredMixin):
     form_class = CustomUserChangeForm
     success_url = reverse_lazy('project_list')
     template_name = 'profile/profile_edit.html'
@@ -27,7 +28,7 @@ class ChangeProfileView(UpdateView):
     def get_object(self):
         return self.request.user
 
-class UpdateProfileView(UpdateView):
+class UpdateProfileView(UpdateView, LoginRequiredMixin, UserPassesTestMixin):
     model = Profile
     template_name = 'profile/bio_update.html'
 
